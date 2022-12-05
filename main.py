adbb835d51860c7b0065b29c8ee5fb2f39b02d6a
Felipe <tomazalvesfelipe@gmail.com>
2022-12-05 00:07:46 -0400

    implementando o banco e o metodo post

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from flask_marshmallow import Marshmallow
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
+
 
 class Carro(db.Model):
     __tablename__ = "Carros"
@@ -19,14 +20,14 @@ class Carro(db.Model):
     cor = db.Column(db.String(255))
     tipo = db.Column(db.String(255))
 
-
     def __repr__(self):
         return '<Carro %s' % self.marca
+
 
 with app.app_context():
     db.create_all()
     db.session.commit()
-    
+
 
 class CarroSchema(ma.Schema):
     class Meta:
@@ -37,19 +38,20 @@ class CarroSchema(ma.Schema):
 carro_schema = CarroSchema()
 carros_schema = CarroSchema(many=True)
 
+
 class ListaCarros(Resource):
     def get(self):
         carros = Carro.query.all()
         return carros_schema.dump(carros)
-    
+
     def post(self):
         novo_carro = Carro(
-                marca=request.json['marca'],
-                nome=request.json['nome'],
-                ano=request.json['ano'],
-                cor=request.json['cor'],
-                tipo=request.json['tipo']
-                )
+            marca=request.json['marca'],
+            nome=request.json['nome'],
+            ano=request.json['ano'],
+            cor=request.json['cor'],
+            tipo=request.json['tipo']
+        )
         db.session.add(novo_carro)
         db.session.commit()
         return carro_schema.dump(novo_carro)
@@ -57,7 +59,5 @@ class ListaCarros(Resource):
 
 api.add_resource(ListaCarros, '/carros')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
-    
