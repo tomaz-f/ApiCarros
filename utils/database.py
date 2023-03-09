a31d9a81f56f1cf29a6e25735e6d999555c2e3b5
Felipe <tomazalvesfelipe@gmail.com>
2023-03-09 10:09:51 -0400

    REFACTOR: alterando banco de dados

--- utils/database.py
+++ utils/database.py
@@ -5,10 +5,9 @@ db = SQLAlchemy()
 
 def config_database(app):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+mariadbconnector://root:123456@localhost:3306/carros'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://admin:admin@localhost:5432/carros'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     with app.app_context():
         db.create_all()
-
